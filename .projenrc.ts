106b81072283d84596c60568678b60c84f53e738
Momo Kornher <kornherm@amazon.co.uk>
2025-02-20 15:28:02 +0000

    chore: enable vscode workspace

--- .projenrc.ts
+++ .projenrc.ts
@@ -105,23 +105,16 @@ const repo = configureProject(
     ],
 
     eslintOptions: {
-      // prettier: true,
       dirs: ['lib'],
       devdirs: ['test'],
       ignorePatterns: ['resources/**/*.ts'],
     },
-
-    /*
-    // Too many files don't match prettier
-    prettier: true,
-    prettierOptions: {
-      settings: {
-        printWidth: 120,
-        singleQuote: true,
-        trailingComma: pj.javascript.TrailingComma.ALL,
-      },
+    
+    vscodeWorkspace: true,
+    vscodeWorkspaceOptions: {
+      includeRootWorkspace: true,
     },
-    */
+
     workflowNodeVersion: 'lts/*',
     workflowRunsOn,
     gitignore: ['.DS_Store'],
