cff25c2dfdc05d154ee3759a26d33aeba86b7462
iliapolo <eli.polonsky@gmail.com>
2025-03-13 23:33:25 +0200

    mid work

---
+++ packages/@aws-cdk-testing/cli-integ/tests/cli-integ-tests/cdk-migrate-deploys-successfully.integtest.ts
@@ -0,0 +1,27 @@
+import { DescribeStacksCommand } from "@aws-sdk/client-cloudformation";
+import { integTest, withCDKMigrateFixture } from "../../lib";
+
+['typescript', 'python', 'csharp', 'java'].forEach((language) => {
+  integTest(
+    `cdk migrate ${language} deploys successfully`,
+    withCDKMigrateFixture(language, async (fixture) => {
+      if (language === 'python') {
+        await fixture.shell(['pip', 'install', '-r', 'requirements.txt']);
+      }
+
+      const stackArn = await fixture.cdkDeploy(
+        fixture.stackNamePrefix,
+        { neverRequireApproval: true, verbose: true, captureStderr: false },
+        true,
+      );
+      const response = await fixture.aws.cloudFormation.send(
+        new DescribeStacksCommand({
+          StackName: stackArn,
+        }),
+      );
+
+      expect(response.Stacks?.[0].StackStatus).toEqual('CREATE_COMPLETE');
+      await fixture.cdkDestroy(fixture.stackNamePrefix);
+    }),
+  );
+});
